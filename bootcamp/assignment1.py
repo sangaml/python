c129b1b1a379764280799650279b304aa74cc00b
Sangam Lonkar <Sangam.Lonkar1@ey.com>
2019-10-16 11:35:04 +0530

    added assignment1

--- bootcamp/assignment1.py
+++ bootcamp/assignment1.py
@@ -1,1 +1,19 @@
-#About to start
+def myfunc(i):
+    for j in range(i):
+        for k in range(j, i):
+            print(" ", end ="")
+        if j>1:
+            for l in range(j):
+                print(l+1, end =" ")
+            rev = j-1
+            while rev>0:
+                print(rev, end =" ")
+                rev -= 1
+
+        else:   
+             for l in range(j+1):
+                print(l+1, end =" ")
+
+        print("\n", end ="")
+
+myfunc(10)
