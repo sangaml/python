18ffa61184ae770bb5793596975ab6aafd642a5e
Sangam Lonkar <Sangam.Lonkar1@ey.com>
2019-10-18 15:30:06 +0530

    started assignment5

--- bootcamp/assignment5.py
+++ bootcamp/assignment5.py
@@ -1,5 +1,32 @@
-content1 = input("Enter content to add:")
-file3 = open('test.py', 'a+')
-file3.write(content1)
-file3.close() 
+class Vehicle():
+    needsmaintenace = False
+    tripSincemaintenance = 0
 
+    def __init__(self,make,model,weight,year):
+    
+        self.make = make
+        self.model = model
+        self.weight = weight
+        self.year = year
+
+    def NeedsMaintenace(self):
+        pass
+
+    def TripSinceMaintenance(self):
+        pass
+
+class Cars(Vehicle):
+
+    def __init__(self):
+        #Vehicle.__init__(self)
+        pass
+
+    def Drive (self,IsDriving='True'):
+        pass
+        
+
+    def stop (self,IsDriving='False'):
+        pass
+
+    def repair (self):
+        pass
